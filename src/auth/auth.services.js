4da4cff1dcafad68f6b85a21e3fb1ab91f9288a3
Ricardo Hoyos <rimaylu@gmail.com>
2023-02-24 21:15:35 -0500

    feature: add env variables to my project-rh

--- src/auth/auth.services.js
+++ src/auth/auth.services.js
@@ -1,24 +1,25 @@
 const checkUserCredentials = require('./auth.controller')
 const response = require('../utils/handleResponses')
 const jwt = require('jsonwebtoken')
+const config = require('../../config').api
 
 const postLogin = (req, res) => {
     const { email, password } = req.body
     checkUserCredentials(email, password)
         .then(data => {
-            if(data){
-                
+            if (data) {
+
                 const token = jwt.sign({
                     id: data.id,
                     email: data.email,
                     firstName: data.firstName
-                }, 'academlo', {
+                }, config.secretOrKey, {
                     expiresIn: '1d'
                 })
 
                 response.success({
                     res,
-                    status:200,
+                    status: 200,
                     message: 'Correct Credentials!',
                     data: token
                 })
@@ -33,7 +34,7 @@ const postLogin = (req, res) => {
         .catch(err => {
             response.error({
                 res,
-                status:400,
+                status: 400,
                 data: err,
                 message: 'Something Bad'
             })
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,27 +1,21 @@
-//? Importamos de passport-jwt las 2 cositas de aqui abajo
 const { ExtractJwt, Strategy } = require('passport-jwt')
-//? Importamos de passport el core completo
 const passport = require('passport')
 
-//? Importamos nuestro controlador que nos va a permitir validar si el usuario existe en mi db
 const { findUserById } = require('../users/users.controllers')
- 
-//? Generamos configuraciones basicas para manejar passport con jwt
+const config = require('../../config').api
+
 const passportConfigs = {
-    //? Esta configuracion lo que hace es extraer el Bearer Token de mi peticion
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), //? 
-    secretOrKey: 'academlo'
+    secretOrKey: config.secretOrKey
 }
 
-
-//? done()
 passport.use(new Strategy(passportConfigs, (tokenDecoded, done) => {
     findUserById(tokenDecoded.id)
         .then(data => {
-            if(data){
-               done(null, tokenDecoded) //? El usuario si Existe y es valido
+            if (data) {
+                done(null, tokenDecoded) //? El usuario si Existe y es valido
             } else {
-               done(null, false, {message: 'Token Incorrect'}) //? El usuario no existe
+                done(null, false, { message: 'Token Incorrect' }) //? El usuario no existe
             }
         })
         .catch(err => {
@@ -29,4 +23,4 @@ passport.use(new Strategy(passportConfigs, (tokenDecoded, done) => {
         })
 }))
 
-module.exports = passport.authenticate('jwt', {session: false})
+module.exports = passport.authenticate('jwt', { session: false })
